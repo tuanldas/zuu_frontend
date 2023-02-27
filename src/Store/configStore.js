ab874a7df92c00021e279f7bdc179dd3f4d2b8f3
Lê Đình Tuấn <tuanldas@gmail.com>
2023-02-27 16:56:32 +0700

    Setup store redux

---
+++ src/Store/configStore.js
@@ -0,0 +1,19 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export function configureStore(initialState) {
+    const store = createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(
+            applyMiddleware(...middlewares)
+        )
+    );
+    sagaMiddleware.run(rootSaga);
+    return store;
+}
